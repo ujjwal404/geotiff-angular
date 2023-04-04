d32dc499e8768865ee71f0db91f1543d66a77c24
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2023-04-04 22:54:03 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-declare var Tiff: any;
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as GeoTIFF from 'geotiff';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,19 +9,46 @@ declare var Tiff: any;
 export class AppComponent implements OnInit {
   title = 'angular-image-viewer';
 
+  // viewchild canvas element
+  // @ts-ignore
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+
   ngOnInit(): void {
     this.loadimage();
   }
 
-  loadimage() {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'arraybuffer';
-    xhr.open('GET', '../assets/images/1.tiff');
-    xhr.onload = (e) => {
-      const tiff = new Tiff({ buffer: xhr.response });
-      const canvas = tiff.toCanvas();
-      document.body.append(canvas); // beautify!
-    };
-    xhr.send();
+  async loadimage() {
+    // Load the GeoTIFF file from the assets folder
+    const response = await fetch('../assets/images/5.tiff');
+    const buffer = await response.arrayBuffer();
+    const tiff = await GeoTIFF.fromArrayBuffer(buffer);
+    // Get the first image from the TIFF file
+    const image = await tiff.getImage();
+
+    const width = image.getWidth();
+    const height = image.getHeight();
+
+    const data = await image.readRasters();
+    console.log(width, height);
+
+    // Create a canvas element to render the image on
+    const canvas = this.canvas.nativeElement;
+    canvas.width = width;
+    canvas.height = height;
+
+    // Get the 2D rendering context from the canvas
+    const ctx: any = canvas.getContext('2d');
+
+    // Create an ImageData object
+    const idata = ctx.createImageData(width, height);
+
+    // Set the imagedata from the tiff
+    idata.data.set(data[0]);
+
+    // Draw the imagedata on the canvas
+    ctx.putImageData(idata, 0, 0);
+
+    // Draw the canvas on the page
+    document.body.appendChild(canvas);
   }
 }
