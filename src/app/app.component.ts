b9fff5c4b7ec2fe40015f3e40d71d5287ebd1d40
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2023-04-04 14:35:26 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var Tiff: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-demo';
+export class AppComponent implements OnInit {
+  title = 'angular-image-viewer';
+
+  ngOnInit(): void {
+    this.loadimage();
+  }
+
+  loadimage() {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'arraybuffer';
+    xhr.open('GET', '../assets/images/1.tiff');
+    xhr.onload = (e) => {
+      const tiff = new Tiff({ buffer: xhr.response });
+      const canvas = tiff.toCanvas();
+      document.body.append(canvas); // beautify!
+    };
+    xhr.send();
+  }
 }
