c61960058a9b9140fa153be56ea09ee1abe84b7c
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2023-04-05 13:43:19 +0530

    done with images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
 
   async loadimage() {
     // Load the GeoTIFF file from the assets folder
-    const response = await fetch('../assets/images/5.tiff');
+    const response = await fetch('../assets/images/2.tiff');
     const buffer = await response.arrayBuffer();
     const tiff = await GeoTIFF.fromArrayBuffer(buffer);
     // Get the first image from the TIFF file
@@ -43,7 +43,19 @@ export class AppComponent implements OnInit {
     const idata = ctx.createImageData(width, height);
 
     // Set the imagedata from the tiff
-    idata.data.set(data[0]);
+    // idata.data.set(data[0]);
+    const dataArr: any = data[0];
+    console.log(data[0]);
+
+    // convert float32 to uint8 and display grayscale image
+    for (let i = 0; i < dataArr.length; i++) {
+      const val = dataArr[i];
+      const scaledVal = Math.floor(val * 255);
+      idata.data[i * 4] = scaledVal;
+      idata.data[i * 4 + 1] = scaledVal;
+      idata.data[i * 4 + 2] = scaledVal;
+      idata.data[i * 4 + 3] = 255;
+    }
 
     // Draw the imagedata on the canvas
     ctx.putImageData(idata, 0, 0);
